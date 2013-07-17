788c620639beaf2fa3d6a9144c434a20cc7c8234
Denis Veselov <veselov@trilan.ru>
2013-07-17 16:11:41 +0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name='django-maven',
+    version='0.1',
+    license='ISC',
+    description='Capture exceptions in django management commands '
+                'into Sentry by Raven',
+    long_description=read('README.md'),
+    keywords='django exception management command sentry raven raise error',
+    url='https://github.com/saippuakauppias/django-maven',
+    author='Denis Veselov',
+    author_email='progr.mail@gmail.com',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        'django'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP'
+    ],
+)
